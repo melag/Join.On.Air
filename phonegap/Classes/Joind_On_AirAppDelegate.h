761fadbc270bb0cf08a7581a1782f865a6b37b25
Ideato Srl <ft@ideato.it>
2010-04-09 17:24:41 +0200

    Fist commit for phonegap framework

---
+++ phonegap/Classes/Joind_On_AirAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  Joind_On_AirAppDelegate.h
+//  Joind.On.Air
+//
+//  Created by Francesco Trucchia on 08/04/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface Joind_On_AirAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
